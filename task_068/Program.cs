b3e1b52eb01c6da082d7c9e65b62c6acd1cb6aa8
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-25 21:40:00 +0300

    task068

---
+++ task_068/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите неотрицательное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+while (m < 0)
+{
+    m = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Введите неотрицательное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int A(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if (n == 0)
+        {
+            return A(m - 1, 1);
+        }
+        else
+        {
+            return A(m - 1, A(m, n - 1));
+        }
+    }
+}
+Console.Clear();
+Console.Write($"m = {m}, n = {n} -> A(m,n) = {A(m, n)}");
